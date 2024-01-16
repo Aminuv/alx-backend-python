82318ec16d51704229377703a8088e1e1cd3f567
Aminuv <mohamed.elmarbouh07@gmail.com>
2024-01-16 00:03:51 +0000

    tasks 01

---
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+""" spaw wait_tme n times with the specified max_delay. """
+import asycio
+import random
+from typing import List
+wait_random = __import__("0-basic_async_syntax").wait_random
+
+
+async def wait_n(n: int, max_delay: int) ->List[float]:
+    """ def Function """
+    w_time = await asyncio.gather(
+        *tuple(map(lambda _: wait_random(max_delay), range(n))))
+    return sorted(w_time)
