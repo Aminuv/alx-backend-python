8efdb4d689eb40e816abb80c510435f2b63a78a9
Aminuv <mohamed.elmarbouh07@gmail.com>
2024-01-16 17:50:03 +0000

    Fix tasks 01

---
+++ 0x02-python_async_comprehension/1-async_comprehension.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+""" function takes no arguments """
+from typing import List
+async_generator = __import__("0-async_generator").async_generator
+
+
+async def async_comprehension() -> List[float]:
+    """ List 10 Numbers """
+    return [rand async for rand in async_comprehension()]
