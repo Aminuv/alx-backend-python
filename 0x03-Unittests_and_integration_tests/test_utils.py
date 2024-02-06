cdd2c5cc38b3458ed0b3747f3e60076dab0a2347
Aminuv <mohamed.elmarbouh07@gmail.com>
2024-02-06 00:24:35 +0000

    fix tasks 03

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -26,14 +26,9 @@ class TestAccessNestedMap(unittest.TestCase):
     @parameterized.expand([({}, ("a",)), ({"a": 1}, ("a", "b"))])
 
     def test_access_nested_map_exception(
-
         self, nested_map: Dict[str, Any], path: Tuple[str]
-
     ) -> None:
-
-
         with self.assertRaises(KeyError):
-
             access_nested_map(nested_map, path)
 
 class TestGetJson(unittest.TestCase):
